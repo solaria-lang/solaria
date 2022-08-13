a6d67ab852128ec960249bfd2d6f0bc6c662287b
Marcelo Fernandes <marceelofernandes@gmail.com>
2022-08-13 15:32:14 +1200

    Add tests cases for arithmetic statements
    This commits introduces 5 arithmetic instructions to test Solaria.
    These instructions are simple equations that resolve to a particular
    number.
    
    They test, but also expose, the limitations of having a left-to-right
    evaluation (when we don't have parenthesis). For example, the following
    equations results in an unexpected value:
    
    `1 + 2 * 3 = 9`
    
    This will be fixed once we can parse the language better. At that stage,
    we'll understand which operators take precedence over others.

--- main.c
+++ main.c
@@ -3,28 +3,121 @@
 #include "debug.h"
 #include "vm.h"
 
+void arithmetic_instruction_1();
+void arithmetic_instruction_2();
+void arithmetic_instruction_3();
+void arithmetic_instruction_4();
+void arithmetic_instruction_5();
+
 
 int main(int argc, const char* argv[]) {
   init_vm();
 
+  arithmetic_instruction_1(); // -0.82
+  arithmetic_instruction_2(); // 5
+  arithmetic_instruction_3(); // 9
+  arithmetic_instruction_4(); // 0
+  arithmetic_instruction_5(); // -1
+
+  free_vm();
+  return 0;
+}
+
+
+void arithmetic_instruction_1() {
+  // This test represents the equation -( (1.2 + 3.4) / 5.6 ) = -0.82...
   chunk_t chunk;
+  int line = 777;
   init_chunk(&chunk);
 
-  int line = 777;
-
-  // This test represents -( (1.2 + 3.4) / 5.6 ) = -0.82...
   write_constant(&chunk, 1.2, line);
   write_constant(&chunk, 3.4, line);
   write_chunk(&chunk, OP_ADD, line);
   write_constant(&chunk, 5.6, line);
   write_chunk(&chunk, OP_DIVIDE, line);
   write_chunk(&chunk, OP_NEGATE, line);
-  write_chunk(&chunk, OP_RETURN, 777);
+  write_chunk(&chunk, OP_RETURN, line);
 
-  // disassemble_chunk(&chunk, "test chunk");
   interpret(&chunk);
-
-  free_vm();
   free_chunk(&chunk);
-  return 0;
+}
+
+
+void arithmetic_instruction_2() {
+  // This test represent the equation 1 * 2 + 3 = 5
+  chunk_t chunk;
+  int line = 321;
+  init_chunk(&chunk);
+
+  write_constant(&chunk, 1, line);
+  write_constant(&chunk, 2, line);
+  write_chunk(&chunk, OP_MULTIPLY, line);
+  write_constant(&chunk, 3, line);
+  write_chunk(&chunk, OP_ADD, line);
+  write_chunk(&chunk, OP_RETURN, line);
+
+  interpret(&chunk);
+  free_chunk(&chunk);
+}
+
+
+void arithmetic_instruction_3() {
+  // This test represent the equation 1 + 2 * 3 = 9
+  // (remember stack works from left to right)
+  chunk_t chunk;
+  int line = 123;
+  init_chunk(&chunk);
+
+  write_constant(&chunk, 1, line);
+  write_constant(&chunk, 2, line);
+  write_chunk(&chunk, OP_ADD, line);
+  write_constant(&chunk, 3, line);
+  write_chunk(&chunk, OP_MULTIPLY, line);
+  write_chunk(&chunk, OP_RETURN, line);
+
+  interpret(&chunk);
+  free_chunk(&chunk);
+}
+
+
+void arithmetic_instruction_4() {
+  // This test represent the equation 3 - 2 - 1 = 0
+  chunk_t chunk;
+  int line = 555;
+  init_chunk(&chunk);
+
+  write_constant(&chunk, 3, line);
+  write_constant(&chunk, 2, line);
+  write_chunk(&chunk, OP_SUBTRACT, line);
+  write_constant(&chunk, 1, line);
+  write_chunk(&chunk, OP_SUBTRACT, line);
+  write_chunk(&chunk, OP_RETURN, line);
+
+  interpret(&chunk);
+  free_chunk(&chunk);
+}
+
+
+void arithmetic_instruction_5() {
+  // This test represent the equation 1 + 2 * 3 - 4 / -5 = -1
+  // (remember stack works from left to right)
+  chunk_t chunk;
+  int line = 555;
+  init_chunk(&chunk);
+
+  write_constant(&chunk, 1, line);
+  write_constant(&chunk, 2, line);
+  write_chunk(&chunk, OP_ADD, line);
+  write_constant(&chunk, 3, line);
+  write_chunk(&chunk, OP_MULTIPLY, line);
+  write_constant(&chunk, 4, line);
+  write_chunk(&chunk, OP_SUBTRACT, line);
+  write_constant(&chunk, 5, line);
+  write_chunk(&chunk, OP_NEGATE, line);
+  write_chunk(&chunk, OP_DIVIDE, line);
+
+  write_chunk(&chunk, OP_RETURN, line);
+
+  interpret(&chunk);
+  free_chunk(&chunk);
 }
